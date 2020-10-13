b94e599766b75cddd3b32b9b066e94d17d02e20f
krishna <krishnabanshla25@gmial.com>
2020-10-13 15:52:29 +0530

    Confirm

---
+++ Frontend/src/app/Property/Property-card/Property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+   selector:'app-property-card',
+  // template: `<H1>
+  //   I am a card
+  //    </H1>`
+  templateUrl:'Property-card.component.html',
+  styleUrls:['Property-card.component.css']
+
+})
+
+export class PropertyCardComponent
+{
+
+}
